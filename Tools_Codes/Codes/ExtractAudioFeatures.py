1f2a64c06ac20da7e1942a1f0d4a60b631e1ced7
KausikN <coe17b010@iiitdm.ac.in>
2019-09-22 13:25:07 +0530

    Changes

--- Tools_Codes/Codes/ExtractAudioFeatures.py
+++ Tools_Codes/Codes/ExtractAudioFeatures.py
@@ -2,6 +2,8 @@ import numpy as np
 import librosa
 import os
 import glob
+import matplotlib.pyplot as plt
+import pylab
 
 def windows(data, window_size):
 	start = 0
@@ -14,7 +16,7 @@ def extract_features(parent_dir, sub_dirs, file_ext="*.wav", bands = 128, frames
 	log_specgrams = []
 	labels = []
 	ITJ = 0
-	print ("Started")
+	print ("Started Extracting Features...")
 	for l, sub_dir in enumerate(sub_dirs):
 		print ("In subdir: ", sub_dir)
 		PTJ = 1
@@ -25,8 +27,7 @@ def extract_features(parent_dir, sub_dirs, file_ext="*.wav", bands = 128, frames
 				if(len(sound_clip[int(start):int(end)]) == window_size):
 					signal = sound_clip[int(start):int(end)]
 					melspec = librosa.feature.melspectrogram(signal, n_mels = bands)
-					logspec = librosa.amplitude_to_db(melspec)		# MISSING LOGAMPLITUDE
-					logspec = melspec
+					logspec = librosa.amplitude_to_db(melspec)
 					logspec = logspec.T.flatten()[:, np.newaxis].T
 					log_specgrams.append(logspec)
 					labels.append(label)
@@ -34,11 +35,44 @@ def extract_features(parent_dir, sub_dirs, file_ext="*.wav", bands = 128, frames
 			PTJ = PTJ + 1
 		ITJ = ITJ + 1
 	log_specgrams = np.array(log_specgrams)
-	print ("Done")
+	print ("Finished Extracting Features")
 	print (log_specgrams.shape)
 	log_specgrams = np.asarray(log_specgrams).reshape(len(log_specgrams), bands, frames)
 	features = log_specgrams
-	return np.array(features)
+	return np.array(features), labels
 
-features = extract_features("C:/GitHub Codes and Projects/Projects", ["CrowdIO-Project"], "0.wav")
+path = input("Enter Path of subdirectories: ")
+subdirs = input("Enter Sub Directories: ").split(',')
+features, labels = extract_features(path, subdirs, "*.wav")
+print("\n\n", len(labels), " - Labels:\n\n", labels)
 print("\n\n", features.shape, " - Features:\n\n", features)
+
+# Plotting spectrograms
+subplot_check = input("Do you want subplots: ")
+
+if subplot_check == "Y":
+	n_columns = int(input("Enter no of columns in plot: "))
+	fig, axs = plt.subplots(int(features.shape[0]/n_columns) + 1, n_columns)
+
+	i = 0
+	j = 0 
+	for imgindex in range(features.shape[0]):
+		axs[i, j].imshow(features[imgindex], cmap=pylab.get_cmap('YlGnBu'))
+		axs[i, j].set_title("Axis [" + str(i) + ", " + str(j) + "]")
+		j = j + 1
+		if j == n_columns:
+			j = 0
+			i = i + 1
+
+	for ax in axs.flat:
+		ax.set(xlabel="bands", ylabel="frames")
+
+	# Hide x labels and tick labels for top plots and y ticks for right plots.
+	for ax in axs.flat:
+		ax.label_outer()
+else:
+	for imgindex in range(features.shape[0]):
+		fig = plt.figure(imgindex)
+		plt.imshow(features[imgindex], cmap=pylab.get_cmap('YlGnBu'))
+
+plt.show()
